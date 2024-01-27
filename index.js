195495420b489611ff0d53968420e0cc29dabd24
Hareth Alrawi <157896813+JKR9@users.noreply.github.com>
2024-01-27 17:30:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching OmarFive Server","Listening to Made by kr_j"];
 
 
 let currentIndex = 0;
